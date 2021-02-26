8c98f5496df4a72ef2ab22d3e1134703ab9040f5
Parag's Pc <parag6165@gmail.com>
2021-02-26 18:38:38 +0530

    update some ui in

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 //it is a client side js file
-console.log('commit')
+//console.log('client side js running')
  //dom manipulation get access to html 
  const weatherform = document.querySelector('form');
  const search = document.querySelector('input');
@@ -8,25 +8,35 @@ console.log('commit')
 //adding event listener
  weatherform.addEventListener('submit',(e)=> {
         e.preventDefault();
-        const location = search.value;
-
+        const location =  search.value;
+        console.log(typeof location)
       message_1.textContent = 'Loading...'
       message_2.textContent = ''
          
- //use fetch api to get json data and show it in clint side ui       
-fetch('/weather?search='+ location).then((response) =>{
-      
-    response.json().then((data) => {
+ //use fetch api to get json data and show it in clint side ui
+   try{ 
+     if(location === "" )  return message_1.textContent= 'Trouble In Finding Your Loaction!!Please Try Another One' 
+   
+   
+     fetch('/weather?search='+  location).then((response) =>{
+    
+   
+   response.json().then((data) => {
       //handle error
+      console.log(data.error)
       if(data.error) {
          
-        message_1.textContent ='trouble in finding your location please try another one';
-      }else{
+        message_1.textContent ='Trouble in finding your location please try another one';
+     }else{
             
-          message_1.textContent = data.location;
+          message_1.textContent = data.location
           message_2.textContent = data.forcast;
-        }
+       }
      })
    })
-       
+   
+  }catch(e) {
+     message_1.textContent = 'please fill the right location'
+  }
+ 
 })
